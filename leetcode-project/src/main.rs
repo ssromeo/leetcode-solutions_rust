68ec891c49d1ddcfa689fd107276f699d9fc2841
Suitox93 <ssilvacontact@gmail.com>
2026-06-04 10:50:48 +0700

    Correction du dossier leetcode-project

---
+++ leetcode-project/src/main.rs
@@ -0,0 +1,28 @@
+
+struct Solution;
+use std::collections::HashMap;
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut nums_map : HashMap<i32,i32> = HashMap::new();
+        // 3,2,5,9 target 7  temp = 4
+        for(index_nums,&num) in nums.iter().enumerate(){
+            let temp = target - num;
+            if nums_map.contains_key(&temp) {
+                println!(" {}, {} " ,*nums_map.get(&temp).unwrap() ,index_nums as i32);
+                return vec![*nums_map.get(&temp).unwrap(),index_nums as i32, ];
+
+            }
+            nums_map.insert(num, index_nums as i32);
+        }
+        return vec![];
+
+
+     
+    }
+
+    
+}
+fn main() {
+    Solution::two_sum(vec![3,2,5,9], 7);
+println!("Hello, world!");
+}
